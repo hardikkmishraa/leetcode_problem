779ab1bcbf6eaa35070a2d2fb47e5dad7e80a828
Hardik Mishra <143869414+hardikkmishraa@users.noreply.github.com>
2026-07-29 11:10:08 +0530

    Time: 7 ms (40.02%), Space: 20 MB (99.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,11 +5,17 @@ class Solution:
         """
         n=len(nums)
 
+        if len(nums)==0: #edge case
+            return
+        
+
         i=0
         while i<len(nums):
             if nums[i]==0:
                 break
             i+=1
+        if i==len(nums): #edge case
+            return
         
         j=i+1
         while j<len(nums):
