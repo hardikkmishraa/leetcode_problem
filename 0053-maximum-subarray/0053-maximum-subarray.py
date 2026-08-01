e30919613b32919d55c45052e2ccaff42c2b0760
Hardik Mishra <143869414+hardikkmishraa@users.noreply.github.com>
2026-08-01 15:18:43 +0530

    Time: 36 ms (54.79%), Space: 31.5 MB (43.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,11 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        maxi=float("-inf")
-        total=0
+        n=len(nums)
+        maxi=float("-inf")#agr sb minus hua to usko handle krne ke liye, min value sbse
+        total=0 #isme temp store karenge aur capre karnege amxi se , agr bda hua to maxi me update karnege
+
         for i in range(0,n):
             total=total+nums[i]
             maxi=max(maxi,total)
             if total<0:
                 total=0
         return maxi
+#kadane algo-> total me kabi bhi -ve value aata h to usko 0 pr reset krdenge har baar aur sife +ve value to hi add kakre maxi me store karenge ...
+
