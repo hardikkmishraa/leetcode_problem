6e0368487f27a9b880fb0572aff1d84de7994e95
Hardik Mishra <143869414+hardikkmishraa@users.noreply.github.com>
2026-09-01 14:36:04 +0530

    Time: 0 ms (100%), Space: 20.4 MB (66.26%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,11 +6,13 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         temp=head
-        prev=None
+        stack=[]
         while temp is not None:
-            front=temp.next
-            temp.next=prev
-            prev=temp
-            temp=front
-        return prev
-        
+            stack.append(temp.val)
+            temp=temp.next
+        temp=head
+        while temp is not None:
+            e=stack.pop()
+            temp.val=e
+            temp=temp.next
+        return head
